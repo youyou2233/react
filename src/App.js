8bcd117c98cf2ae9eff19edc110e6ef63b139b47
unknown <2968889186@qq.com>
2025-02-28 22:46:37 +0800

    finish deleting tasks and sycing completed status

--- src/App.js
+++ src/App.js
@@ -15,10 +15,28 @@ export default function App(props) {
 //   array.add(mapTaskToName(props.tasks[i]));
 // }
 const [tasks, setTasks] = useState(props.tasks);
-
+//注意：useState左边数组的两个元素的名字是自定义的，但是无论元素名字怎样，功能是不变的
 function addTask(name) {
   const newTask = { id: `todo-${nanoid()}`, name, completed: false };
   setTasks([...tasks, newTask]);//省略号代表的是复制现有的任务列表，确保不修改原数组
+}
+
+function toggleTaskCompleted(id) {
+  const updatedTasks = tasks.map((task) => {
+    // if this task has the same ID as the edited task
+    if (id === task.id) {
+      // use object spread to make a new object
+      // whose `completed` prop has been inverted
+      return { ...task, completed: !task.completed };
+    }
+    return task;
+  });
+  setTasks(updatedTasks);
+}
+
+function deleteTask(id) {
+  const remainingTasks = tasks.filter((task) => id !== task.id);
+  setTasks(remainingTasks);
 }
 
 const taskList = tasks.map((task) => (
@@ -27,8 +45,16 @@ const taskList = tasks.map((task) => (
     name={task.name}
     completed={task.completed}
     key={task.id}//规定无序列表要有key
+    toggleTaskCompleted={toggleTaskCompleted}
+    deleteTask={deleteTask}
   />
 ));
+
+const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+const headingText = `${taskList.length} ${tasksNoun} remaining`;
+//上一行也可以将字符串片段放入数组，然后用join()方法拼接
+//如：const headingText=[taskList.length,tasksNoun,"remaining"].join(" ");
+
 //有几个就映射几个Todo，要加Todo只用在index.js的DATA里面添加数据即可；Todo.js向App.js里面传组件，index.js向App.js里面传数组数据
   return (
     <div className="todoapp stack-large">
@@ -39,7 +65,7 @@ const taskList = tasks.map((task) => (
       <FilterButton filterName="Active"/>
       <FilterButton filterName="Completed"/>
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading"> {headingText} </h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
--- src/components/Form.js
+++ src/components/Form.js
@@ -6,13 +6,17 @@ function Form(props) {
     const [name, setName] = useState(" ");
 
     function handleSubmit(event){
-        event.preventDefault();
-        props.onSubmit(name);
-        setName("");
+        event.preventDefault();// 阻止表单的默认提交行为
+        props.onSubmit(name);  // 调用父组件传递的函数（如 App.js 中的 addTask
+        //这个props.onSubmit函数的值是App.js里面的addTask函数
+        setName(""); // 清空输入框
     }
     function handleChange(event) {
         setName(event.target.value);
     }
+    //注意：handleChange 和 handleSubmit 不会同时触发原因：
+    //handleChange 是由输入框的 onChange 事件触发的，每次用户输入时都会调用。作用是将输入框的值实时同步到 React 状态中。
+    //handleSubmit 是由表单的 onSubmit 事件触发的，只有当用户点击提交按钮时才会调用，作用是处理表单提交的逻辑。
   return (
     <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
@@ -36,3 +40,10 @@ function Form(props) {
   );
 }
 export default Form;
+
+//写一下思路流程：1、用户在输入框form中输入内容，该event触发onChange事件，并将事件对象传递给handleChange函数
+//2、获取event事件中的值（是event.target.value）传给setName，setName将name的值实时更新到输入框中显示
+//3、之后用户点击add按钮，触发onSubmit，调用handleSubmit函数，从而将name传给addTask函数，同时清空输入框
+//4、在App.js中，index.js传入了tasks作为useState的初始值，后根据Form.js传入的name设立了对象类型的变量newTask
+//5、setTasks将原任务列表复制，将newTask加到任务列表后面，形成新的任务列表，更新tasks的值
+//6、
